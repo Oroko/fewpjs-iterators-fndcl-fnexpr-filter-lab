81499942e0136acc68979e2f4b3b3769c0def3d1
Oroko <aoroko09@gmail.com>
2021-04-18 11:38:22 -0400

    commit tree 6a57e6d93917c32a93bfe68533f6c506e8fc4d21

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+// Code your solution here 
+
+const findMatching = (arr, string) => {
+  return arr.filter(e => {
+    return e.toLowerCase() === string.toLowerCase()
+  })
+}
+
+const fuzzyMatch = (arr, string) => {
+  return arr.filter(e => {
+    if (e.slice(0,2) === string.slice(0,2)){
+      return e
+    }
+  })
+}
+
+const matchName = (arr, string) => {
+  return arr.filter(e => e.name === string)
+}
